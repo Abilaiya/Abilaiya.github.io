910966b5f77eda11def1b84910607893a4d52321
Abilaiya <48633540+Abilaiya@users.noreply.github.com>
2019-03-17 23:59:32 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dealer_Portal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://covercheck.vwfsinsuranceportal.co.uk");
+            IWebElement element = driver.FindElement(By.Name("vehicleReg"));
+            element.SendKeys("OV12UYY");
+            element = driver.FindElement(By.Name("btnfind"));
+            element.Click();
+        }
+    }
+}
